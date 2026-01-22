802a219f3943c7e4df0caedff7a94d2aeed244b7
Glitched Nexus <143273662+GlitchedNexus@users.noreply.github.com>
2026-01-21 17:16:12 -0800

    Add kmedians

---
+++ src/lattice/kmedians.py
@@ -0,0 +1,36 @@
+import numpy as np
+from kmeans import KMeans
+
+
+# If you want, you could write this function to compute pairwise L1 distances
+def l1_distances(X1, X2):
+    # Implementation is based on the euclidean_dist_squared function
+    # in utils.py.
+
+    return np.sum(np.abs(X1[:, None, :] - X2[None, :, :]), axis=2)
+
+
+class KMedians(KMeans):
+    # We can reuse most of the code structure from KMeans, rather than copy-pasting,
+    # by just overriding these few methods. Object-orientation!
+
+    def get_assignments(self, X):
+        # This implementation is based on the implementation
+        # for the get_assignments() method in KMeans.
+        D1 = l1_distances(X, self.w)
+        return np.argmin(D1, axis=1)
+
+    def update_means(self, X, y):
+        # This implementation is based on the implementation
+        # for the update_means() method in KMeans.
+        for k_i in range(self.k):
+            matching = y == k_i
+            if matching.any():
+                self.w[k_i] = np.median(X[matching], axis=0)
+
+    def loss(self, X, y=None):
+        w = self.w
+        if y is None:
+            y = self.get_assignments(X)
+
+        return np.sum(np.abs(X - w[y]))
